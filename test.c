c724954202aec77232e7ea131713aaafa7fa557f
Aetolis <12118983+Aetolis@users.noreply.github.com>
2022-03-05 16:21:11 -0500

    Fix unit tests

--- test.c
+++ test.c
@@ -7,37 +7,40 @@
 typedef uint32_t crc_t;
 
 // l1 function declarations
-extern crc_t crc_table[256];
-extern void l3_init();
-extern crc_t calc_crc(char *buffer, int length);
-extern bool check_crc(char *buffer, int length);
+extern void l3_init(crc_t* crc_table);
+extern crc_t calc_crc(char *buffer, int length, crc_t* crc_table);
+extern bool check_crc(char *buffer, int length, crc_t* crc_table);
 
 // l2 function declarations
 extern int build_l4_packet(char* buffer, int* length, char* name, int nameLength, char* value, int valueLength);
 extern int read_l4_packet(char* buffer, char* name, int* nameLength, char* value, int* valueLength);
 
 void test_calc_crc() {
-    l3_init();
+    crc_t crc_table[256];
+    l3_init(crc_table);
+
     char *buf = "123456789";
-    assert((crc_t) calc_crc(buf, strlen(buf)) == 0x89A1897F);
+    assert((crc_t) calc_crc(buf, strlen(buf), crc_table) == 0x89A1897F);
     buf = "fsdjkfsdafdsdsf";
-    assert((crc_t) calc_crc(buf, strlen(buf)) == 0xC664EABF);
+    assert((crc_t) calc_crc(buf, strlen(buf), crc_table) == 0xC664EABF);
     buf = "ruerweojdcdnsci212332";
-    assert((crc_t) calc_crc(buf, strlen(buf)) == 0xF27C4D33);
+    assert((crc_t) calc_crc(buf, strlen(buf), crc_table) == 0xF27C4D33);
     printf("Test calc_crc passed!\n");
 }
 
 void test_check_crc() {
-    l3_init();
+    crc_t crc_table[256];
+    l3_init(crc_table);
+
     char buf[13] = {'1','2','3','4','5','6','7','8','9', 0x89, 0xA1, 0x89, 0x7F};
-    assert(check_crc(buf, 13));
+    assert(check_crc(buf, 13, crc_table));
     buf[4] = 6;
-    assert(check_crc(buf, 13) == false); 
+    assert(check_crc(buf, 13, crc_table) == false); 
     buf[11] = 0x88;
-    assert(check_crc(buf, 13) == false);
+    assert(check_crc(buf, 13, crc_table) == false);
 
     char buf2[12] = {'a', 'b', 'c', 'd', '6', '4', '5', '7', 0x59, 0x82, 0x68, 0x0B};
-    assert(check_crc(buf2, 12));
+    assert(check_crc(buf2, 12, crc_table));
     
     printf("Test check_crc passed!\n");
 }
